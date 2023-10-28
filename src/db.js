17e9876e3ef9f92634c243a5ed95bcdaece3228a
fsquaglia <fernandosquaglia@gmail.com>
2023-10-28 20:14:17 -0300

    redefiniendo01

--- src/db.js
+++ src/db.js
@@ -6,19 +6,19 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY_RAILWAY, DB_DEPLOY_RENDER } =
   process.env;
 
 //!activar para usar conectando a localhost
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+// const sequelize = new Sequelize(
+//   DB_DEPLOY_RAILWAY,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
 
 //! deploy en railway
-// const sequelize = new Sequelize(DB_DEPLOY_RAILWAY, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+const sequelize = new Sequelize(DB_DEPLOY_RAILWAY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
 //! deploy en render
 // const sequelize = new Sequelize(DB_DEPLOY_RENDER, {
